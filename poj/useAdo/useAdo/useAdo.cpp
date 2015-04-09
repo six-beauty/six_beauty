c97c51bd079ad407a3d720d1c37a875f53537a63
six-beauty <sanyue9394@163.com>
2015-04-09 10:34:22 +0800

    work commit

--- poj/useAdo/useAdo/useAdo.cpp
+++ poj/useAdo/useAdo/useAdo.cpp
@@ -4,6 +4,7 @@
 #include "stdafx.h"
 #include "DataBase.h"
 #include "useAdo.h"
+#include "debug.h"
 
 bool InitInstance()
 {
@@ -26,6 +27,35 @@ bool unInitInstance()
 	return true;
 }
 
+void myRun(CAdoDataBase *pDataBase)
+{
+	try
+	{
+		pDataBase->ClearAllParameters();
+		pDataBase->SetSPName("GSP_GP_LoadGameRoomConfig");
+		pDataBase->AddParamter(_TEXT("RETURN_VALUE"),adParamReturnValue,adInteger,sizeof(long),_variant_t(0L));
+		pDataBase->AddParamter(_TEXT("@serverID"),adParamInput,adInteger,sizeof(long),_variant_t(7794L));
+		pDataBase->ExecuteCommand(true);
+
+		long lRetValue = pDataBase->GetReturnValue();
+		long kindID;
+		char roomName[64]={0};
+		pDataBase->GetFieldValue(_TEXT("KindID"),kindID);
+		pDataBase->GetFieldValue(_TEXT("RoomName"),roomName,sizeof(roomName));
+		pDataBase->CloseRecordset();
+	//	CString valMsg;
+	//	valMsg.Format("查询到房间kindID:%d,房间名：%s",kindID,roomName);
+	//	std::cout<<valMsg<<std::endl;
+	}
+	catch (IADOError* pIADOError)
+	{
+		CString errmsg;
+		errmsg.Format(_T("数据库操作发生异常:%s"),pIADOError->GetErrorDescribe());
+		AfxMessageBox(errmsg);
+	}
+
+}
+
 int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 {
 	InitInstance();
@@ -33,8 +63,12 @@ int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 	CAdoDataBase *pDataBase = new CAdoDataBase();
 	pDataBase->SetConnectionInfo("192.168.3.214",1433,"LK78TestDB","sixbeauty","sanyue9394");
 	pDataBase->OpenConnection();
-
+	ConstructOutput();
+	for(int i=0;i<100;++i)
+		myRun(pDataBase);
 	delete pDataBase;
 	unInitInstance();
+	ConstructOutput();
+
 	return 0;
 }
