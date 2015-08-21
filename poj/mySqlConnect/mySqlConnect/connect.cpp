40d39914e53842c30250adce62cc4c1ff0338eff
six-beauty <sanyue9394@163.com>
2015-08-21 17:24:52 +0800

    mysql commit

--- poj/mySqlConnect/mySqlConnect/connect.cpp
+++ poj/mySqlConnect/mySqlConnect/connect.cpp
@@ -1,45 +1,72 @@
 #include "mysql_connection.h"
 #include "mysql_driver.h"
-#include "cppconn/statement.h"
+#include "cppconn/prepared_statement.h"
 #include "stdlib.h"
+#include <crtdbg.h>  
 
 using namespace std;
 
+typedef boost::scoped_ptr<sql::mysql::MySQL_Driver> MySQL_Driver;
+typedef boost::scoped_ptr<sql::Connection> Connection;
+typedef boost::scoped_ptr<sql::PreparedStatement> PreparedStatement;
+typedef boost::scoped_ptr<sql::Statement> Statement;
+typedef boost::scoped_ptr<sql::ResultSet> ResultSet;
+
+#define CRTDBG_MAP_ALLOC  
 int main()
 {
 	sql::mysql::MySQL_Driver *driver;
-	sql::Connection *con;
-	sql::Statement *state;
-	sql::ResultSet *result;
+	Connection con(NULL);
+	PreparedStatement prepareState(NULL);
+	ResultSet result(NULL);
 
 	//初始化驱动
-	driver = sql::mysql::get_mysql_driver_instance();
-	//建立连接
-	con = driver->connect("tcp://127.0.0.1:3306","root","123456");
-
-	if(con->isValid() == false)
+	try
 	{
+		driver=(sql::mysql::get_mysql_driver_instance());
+		//建立连接
+		con.reset(driver->connect("tcp://127.0.0.1:3306","root","123456"));
+
+		if(con->isValid() == false)
+		{
 		cout<<"连接失败"<<endl;
 		return -1;
+		}
+
+		// 查询存储过程
+		prepareState.reset(con->prepareStatement("call test.testproc1(?)"));
+		prepareState->setInt(1,1001);
+		prepareState->executeUpdate();
+
+		result.reset(prepareState->getResultSet());
+		// 输出结果	
+		while(result->next())
+		{
+			int id = result->getInt("id");
+			string name = result->getString("name");
+			cout<<"testuser: "<< id <<" , "<<name<<endl;
+		}
+
+		result->close();
+		prepareState->close();
+		con->close();
+
+		/*
+		result.reset(NULL);
+		prepareState.reset(NULL);
+		con.reset(NULL);
+		*/
+
+		_CrtDumpMemoryLeaks(); 
+
+	//_CrtDumpMemoryLeaks();  
 	}
-	state = con->createStatement();
-
-	state->execute("use test");
-	//查询
-	result = state->executeQuery("select * from test");
-
-	// 输出结果	
-	while(result->next())
+	catch (sql::SQLException &e)
 	{
-		int id = result->getInt("id");
-		string name = result->getString("name");
-		cout<<"testuser: "<< id <<" , "<<name<<endl;
+		cout<<e.what()<<",state:"<<e.getSQLState()<<endl;
+		cout<<"errorCode: " << e.getErrorCode()<<endl;
 	}
-
-	result->close();
-
-	state->close();
-	con->close();
 
 	return 0;
 }
+
